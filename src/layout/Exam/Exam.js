8d62f9eb45706fe0fd47ef1ca5dbac23d84a0ae6
DerpyFisher <giangle.1601hcm@gmail.com>
2023-10-29 10:24:51 +0700

    The Nothing New update
    Tried using useContext but realize that all the context will disappear as soon as the user hit refresh. So stopped use Context as a place to store result and exam.

--- src/layout/Exam/Exam.js
+++ src/layout/Exam/Exam.js
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './style.scss';
 import { useNavigate } from 'react-router-dom';
 import { SubmitButton } from '../../component/Button/SubmitButton/SubmitButton'
 import QuestionForm from '../../component/Form/QuestionForm';
 import Header from './ExamHeader/Header';
 import { shuffle } from '../../utils/shuffleQuestion';
+import {ctx} from '../../CtxData';
 
 export default function Exam() {
     
     // Define useStates.
     const [questionList, setQuestionList] = useState([]);
 
+    // Define useContext
+    const context = useContext(ctx);
+
     // Navigation between tabs.
     const navigation = useNavigate();
 
     const onSubmit = () => {
         localStorage.setItem('finishedTime', Date.now());
+
         navigation('/finished');
     }
 
