d6f8f7ee6b57598babbef7b1520cacfb261fc740
DerpyFisher <giangle.1601hcm@gmail.com>
2023-10-31 02:08:28 +0700

    Update API domains

--- src/const/domain.js
+++ src/const/domain.js
@@ -1,10 +1,8 @@
 /**
- * This file contain all the constants used in the application.
+ * This file contain all the domains we might need in the application.
  * 
  * @author Fernzoe (Derpy_Fisher)
  */
 
-
-export const TEST_DOMAIN = "http://localhost:1880";
-export const TEST_URL = TEST_DOMAIN + "/quizz/1";
-export const PRODUCTION_DOMAIN = "https://server.nglearns.com/quizz/" 
+export const TEST_DOMAIN = "http://localhost:1880"; // For testing purpose.
+export const PRODUCTION_DOMAIN = "https://server.nglearns.com/" // For actual deployement.
