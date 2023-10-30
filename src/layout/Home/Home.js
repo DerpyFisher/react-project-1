40a8914e3973d325ae2d3b11f78ce468a20753f8
DerpyFisher <giangle.1601hcm@gmail.com>
2023-10-31 02:15:48 +0700

    Update deprecated methods
    small changes on how APIs are called on home screen.

--- src/layout/Home/Home.js
+++ src/layout/Home/Home.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react'
+import React, {useState } from 'react'
 import './style.scss'
 import {PRODUCTION_DOMAIN} from '../../const/domain'
 import { SmallTextInput } from  '../../component/Input/SmallText/SmallTextInput'
 import { SubmitButton } from '../../component/Button/SubmitButton/SubmitButton'
 import { useNavigate } from 'react-router-dom'
-import {FetchAPI} from '../../utils/fetchAPI'
+import {getExam} from '../../utils/fetchAPI'
 import Popup from '../../component/Popup/Popup'
 import {emailValidator} from '../../utils/validators'
 import { useEffect } from 'react'
 
 export default function Home() {
-
     // useNavigation for navigating from different tab (screen).
     const navigation = useNavigate();
 
     // useState for gettting and storing current states of the application.
     const [examCode, setExamCode] = useState("");
+    const [email, setEmail] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const [pop, setPop] = useState(false);
-    const [email, setEmail] = useState("");
 
     // Handler for button.
     const HandleButtonClick = async() => {
@@ -27,7 +26,6 @@ export default function Home() {
             return;
         }
         else {
-            const apiCall = await FetchAPI(PRODUCTION_DOMAIN+examCode);
             try {
                 if (examCode.length === 0 || email.length === 0) {
                     setErrorMessage("You haven't fill in the exam code or email.");
@@ -38,19 +36,20 @@ export default function Home() {
                     setPop(true);
                 }
                 else {
-                    const result = await apiCall.json();
+                    const result = await getExam(PRODUCTION_DOMAIN+'quizz/'+examCode);
                     localStorage.setItem('examCode', examCode);
                     localStorage.setItem("email", email);
                     localStorage.setItem('startTime', Date.now());
                     localStorage.setItem("exam", JSON.stringify(result));
+                    localStorage.setItem("result", JSON.stringify([]));
                     navigation('/exam/'+localStorage.getItem("examCode"));
                 }
             } catch (err) {
                 setErrorMessage("Something wrong while getting the exam information!");
+                console.log(err);
                 setPop(true);
-            }
-        }
-    }
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+            }}}
 
     useEffect( () => {
         if (localStorage.getItem("exam") !== null) {
@@ -58,6 +57,7 @@ export default function Home() {
             setPop(true);
         }
     }, [])
+    
     // Return the component.
     return (
         <div className='home-view'> 
