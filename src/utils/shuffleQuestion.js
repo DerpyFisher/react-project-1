46bd59d166532be8a0afbdc1b72774d496e502af
DerpyFisher <giangle.1601hcm@gmail.com>
2023-10-31 02:13:23 +0700

    Add documents.

--- src/utils/shuffleQuestion.js
+++ src/utils/shuffleQuestion.js
@@ -1,27 +1,20 @@
-
 /**
- * 
+ * Shuffle question and their answer up.
  * @param {[]} questionList The list to shuffle up.
- * @returns 
+ * @example
+ * var arr = [2, 11, 37, 42]; shuffle(arr); console.log(arr);
+ * @author Derpy Fisher
+ * @returns  the question list that has been shuffled up.
+ * @version 1.0.0.0
  */
 export function shuffle(questionList) {
     let currentIndex = questionList.length;
     let randomIndex = 0;
-
   while (currentIndex > 0) {
-
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // Thanks, internet strangers!
-    [questionList[currentIndex], questionList[randomIndex]] = [
-      questionList[randomIndex], questionList[currentIndex]];
+    [questionList[currentIndex], questionList[randomIndex]] = [questionList[randomIndex], questionList[currentIndex]];
   }
-
   return questionList;
 }
 
-// Used like so
-var arr = [2, 11, 37, 42];
-shuffle(arr);
-console.log(arr);
